9c8517788a5c87da82573b27382660901c188d8b
Marcus Pinheiro <marcusmagno34@gmail.com>
2022-07-28 15:41:55 -0300

    versão final

--- Avaliação/Program.cs
+++ Avaliação/Program.cs
@@ -10,28 +10,16 @@ namespace Avaliação
     {
         static void Main(string[] args)
         {
-            fatEmpresa();
+            Reversor();
         }
-        static void fatEmpresa()
+        static void Reversor()
         {
-            double sp = 67836.43d,
-                rj = 36678.66d,
-                mg = 29229.88d,
-                es = 27165.48d,
-                anot = 19849.53d,
-                total = ((double) (sp + rj + mg + es + anot)),
-                porc_sp = (Math.Round((double)(sp * 100) / total)),
-                porc_rj = (Math.Round((double)(rj * 100) / total)),
-                porc_mg = (Math.Round((double)(mg * 100) / total)),
-                porc_es = (Math.Round((double)(es * 100) / total)),
-                porc_anot = (Math.Round((double)(anot * 100) / total));
-            Console.WriteLine(total);
-            Console.WriteLine($"A porcentagem do valor mensal de São Paulo para a distribuidora foi de aproximadamente {porc_sp}%");
-            Console.WriteLine($"A porcentagem do valor mensal do Rio de Janeiro para a distribuidora foi de aproximadamente {porc_rj}%");
-            Console.WriteLine($"A porcentagem do valor mensal de Minas Gerais para a distribuidora foi de aproximadamente {porc_mg}%");
-            Console.WriteLine($"A porcentagem do valor mensal de Espirito Santo para a distribuidora foi de aproximadamente {porc_es}%");
-            Console.WriteLine($"A porcentagem do valor mensal dos outros estados para a distribuidora foi de aproximadamente {porc_anot}%");
-            Console.ReadLine();
+            Inicio:
+            Console.Write("Insira o texto a ser invertido: ");
+            string texto = Console.ReadLine(),
+                resultado = new String((new Stack<char>(texto)).ToArray());
+            Console.WriteLine("Assim fica o texto invertido: " + resultado);
+            goto Inicio;
         }
     }
 }
